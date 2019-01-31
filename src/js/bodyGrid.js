a70b0f6468222063f7924f0a35353e0d75d1c7e9
Amber Thomas <ProQuestionAsker@gmail.com>
2019-01-30 19:03:57 -0800

    Working on grid

--- src/js/bodyGrid.js
+++ src/js/bodyGrid.js
@@ -17,7 +17,7 @@ let selectedSize = 'All school sizes'
 let selectedLocale = 'All locales'
 let bodySel = {}
 
-let svgBook = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg>'
+let svgBook = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book-open"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>'
 
 function cleanData(arr){
 	return arr.map((d, i) => {
